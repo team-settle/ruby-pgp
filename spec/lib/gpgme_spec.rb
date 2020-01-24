ddb8302796884b5b96ba1886fd4b38becb2bfcd9
cshtdd <camilin87@gmail.com>
2020-01-24 14:02:33 -0500

    Now I got a test that works on my Mac but not on ubuntu

--- spec/lib/gpgme_spec.rb
+++ spec/lib/gpgme_spec.rb
@@ -1,28 +1,29 @@
 require 'spec_helper'
 require 'gpgme'
+require 'pgp/passphrase_callback'
 
 describe 'gpgme' do
-  let(:private_key_path) { Fixtures_Path.join('private_key_with_passphrase.asc').to_s }
-  let(:public_key_path) { Fixtures_Path.join('public_key_with_passphrase.asc').to_s }
-  let(:passphrase) { 'testingpgp' }
-  let(:unsigned_file) { Fixtures_Path.join('signed_file.txt') }
-  let(:signed_file) { Fixtures_Path.join('signed_file.txt.asc') }
+  it 'can verify a file' do
+    expected_contents = File.read(Fixtures_Path.join('signed_file.txt'))
 
-  it 'can sign a file' do
-    unsigned_data_str = File.read(unsigned_file)
-    private_key_data_str = File.read(private_key_path)
-    expected_signed_data_str = File.read(signed_file)
+    #signed_data = GPGME::Data.from_str(File.read(Fixtures_Path.join('signed_file.txt.asc')))
+    signed_data = File.read(Fixtures_Path.join('signed_file.txt.asc'))
 
-    private_key_data = GPGME::Data.from_str(private_key_data_str)
-    unsigned_data = GPGME::Data.from_str(unsigned_data_str)
+    #expect(1).to eq(2)
 
-    signed_data = GPGME::Data.new
-    GPGME::Ctx.new(password: passphrase) do |ctx|
-      ctx.import_keys(private_key_data)
-      ctx.sign(unsigned_data, signed_data, GPGME::SIG_MODE_CLEAR)
+    crypto = GPGME::Crypto.new
+    #signatures = 0
+    #crypto.verify(signed_data) do |signature|
+    #  expect(signature.valid?).to eq(true)
+    #  signatures += 1
+    #end
+    #expect(signatures).to eq(1)
+
+    output_data = GPGME::Data.empty!
+    crypto.verify(signed_data, output: output_data) do |signature|
+      expect(signature.valid?).to eq(true)
     end
-
-    actual_signed_data_str = signed_data.to_s
-    expect(actual_signed_data_str).to eq(expected_signed_data_str)
+    actual_contents = output_data.to_s
+    expect(actual_contents).to eq(expected_contents)
   end
 end
