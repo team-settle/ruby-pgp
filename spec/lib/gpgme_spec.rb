ffff73aa82f80eff58ca3b068ce23c7813cb4e7f
cshtdd <camilin87@gmail.com>
2020-01-24 14:45:36 -0500

    Cleanup the test a little bit

--- spec/lib/gpgme_spec.rb
+++ spec/lib/gpgme_spec.rb
@@ -11,37 +11,17 @@ describe 'gpgme' do
     end
   end
 
-  #before {
-  #  remove_all_keys
-  #}
-
   it 'can verify a file' do
     expected_contents = File.read(Fixtures_Path.join('signed_file.txt'))
-    #p expected_contents
-    #p '----------------------------------------------------------------'
-
-    #signed_data = GPGME::Data.from_str(File.read(Fixtures_Path.join('signed_file.txt.asc')))
     signed_data = File.read(Fixtures_Path.join('signed_file.txt.asc'))
-    #p signed_data
-    #p '----------------------------------------------------------------'
 
     GPGME::Key.import(File.open(Fixtures_Path.join('public_key_with_passphrase.asc').to_s))
 
     crypto = GPGME::Crypto.new
-    signatures = 0
-    crypto.verify(signed_data) do |signature|
-      #p signature
-      #p '----------------------------------------------------------------'
+    output_data = GPGME::Data.empty!
+    crypto.verify(signed_data, output: output_data) do |signature|
       expect(signature.valid?).to eq(true)
-      signatures += 1
     end
-    expect(signatures).to eq(1)
-
-    #output_data = GPGME::Data.empty!
-    #crypto.verify(signed_data, output: output_data) do |signature|
-    #  expect(signature.valid?).to eq(true)
-    #end
-    #actual_contents = output_data.to_s
-    #expect(actual_contents).to eq(expected_contents)
+    expect(output_data.to_s).to eq(expected_contents)
   end
 end
