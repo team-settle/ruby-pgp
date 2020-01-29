c690f866900b1cbafc67127bc2122400cb96e4a5
cshtdd <camilin87@gmail.com>
2020-01-29 15:21:33 -0500

    Create some infrastructure to determine if we're running gpg2

--- lib/pgp/gpg/runner.rb
+++ lib/pgp/gpg/runner.rb
@@ -2,6 +2,10 @@ require 'open3'
 
 module GPG
   class Runner
+    def is_gpg2?
+      version.start_with? '2.'
+    end
+
     def version
       Open3.popen2e('gpg --version') do |stdin, output, handle|
         return '' unless handle.value.success?
--- spec/lib/pgp/gpg/runner_spec.rb
+++ spec/lib/pgp/gpg/runner_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe GPG::Runner do
   include ProcessHelper
-  
+
   let(:runner) { GPG::Runner.new }
 
   describe 'version' do
@@ -14,6 +14,23 @@ describe GPG::Runner do
     it 'returns empty when gpg fails' do
       setup_process('gpg --version', false, nil)
       expect(runner.version).to eq('')
+    end
+  end
+
+  describe 'is_gpg2?' do
+    it 'returns true when version 2 is default' do
+      allow(runner).to receive(:version).and_return('2.0.22')
+      expect(runner.is_gpg2?).to be_truthy
+    end
+
+    it 'returns false when version 1 is default' do
+      allow(runner).to receive(:version).and_return('1.12.22')
+      expect(runner.is_gpg2?).to be_falsey
+    end
+
+    it 'returns false when there is no gpg' do
+      allow(runner).to receive(:version).and_return('')
+      expect(runner.is_gpg2?).to be_falsey
     end
   end
 end
