13fb746b0cbf0743538c0e458ad4811d46261db8
cshtdd <camilin87@gmail.com>
2020-01-29 16:19:40 -0500

    Extract a GPGMEVersionHelper to help with the gpg version selection

---
+++ lib/pgp/gpg/gpgme_version_helper.rb
@@ -0,0 +1,12 @@
+module GPG
+  class GPGMEVersionHelper
+    def self.switch_to_gpg1
+      runner = GPG::Runner.new
+      if runner.should_switch_to_gpg1?
+        bin = runner.binary_path_gpg1
+        home_dir = GPGME::Engine.dirinfo('homedir')
+        GPGME::Engine.set_info(GPGME::PROTOCOL_OpenPGP, bin, home_dir)
+      end
+    end
+  end
+end
--- spec/lib/gpgme_spec.rb
+++ spec/lib/gpgme_spec.rb
@@ -6,13 +6,7 @@ describe 'gpgme' do
 
   before {
     remove_all_keys
-
-    runner = GPG::Runner.new
-    if runner.should_switch_to_gpg1?
-      bin = runner.binary_path_gpg1
-      home_dir = GPGME::Engine.dirinfo('homedir')
-      GPGME::Engine.set_info(GPGME::PROTOCOL_OpenPGP, bin, home_dir)
-    end
+    GPG::GPGMEVersionHelper.switch_to_gpg1
   }
 
   it 'can verify a file with the correct key' do
