0a253ef8313a375155ac7c2375953dc715e67607
cshtdd <camilin87@gmail.com>
2020-01-29 14:41:40 -0500

    Start building up infrastructure to force GPGME to use GNUPG 1

--- lib/pgp/gpg/runner.rb
+++ lib/pgp/gpg/runner.rb
@@ -1,7 +1,15 @@
+require 'open3'
+
 module GPG
   class Runner
-    def increment(n)
-      n + 1
+    def version
+      Open3.popen2e('gpg --version') do |stdin, output, handle|
+        return '' unless handle.value.success?
+
+        full_version_info = output.gets
+        version_number = full_version_info.lines.first.split(' ').last
+        return version_number
+      end
     end
 
     def add_keys(key_data)
--- spec/lib/pgp/gpg/runner_spec.rb
+++ spec/lib/pgp/gpg/runner_spec.rb
@@ -3,9 +3,21 @@ require 'spec_helper'
 describe GPG::Runner do
   let(:runner) { GPG::Runner.new }
 
-  describe 'increment' do
-    it 'adds 1' do
-      expect(runner.increment(10)).to eq(11)
+  let(:output_stub) { double }
+  let(:exit_code_stub) { double }
+  let(:handle_stub) { double }
+
+  before {
+    allow(handle_stub).to receive(:value).and_return(exit_code_stub)
+  }
+
+  describe 'version' do
+    it 'reads gpg version' do
+      allow(output_stub).to receive(:gets).and_return("gpg (GnuPG) 2.0.22\nlibgcrypt 1.5.3\nblah\nblah")
+      allow(exit_code_stub).to receive(:success?).and_return(true)
+      allow(Open3).to receive(:popen2e).with('gpg --version').and_yield(nil, output_stub, handle_stub)
+
+      expect(runner.version).to eq('2.0.22')
     end
   end
 end
