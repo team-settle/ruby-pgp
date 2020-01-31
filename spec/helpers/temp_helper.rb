08b5d6e3ae29203aa782c6b07165a125aeaeef1e
cshtdd <camilin87@gmail.com>
2020-01-31 16:30:37 -0500

    Break down the TempHelper functionality into smaller easier to reuse methods

--- spec/helpers/temp_helper.rb
+++ spec/helpers/temp_helper.rb
@@ -1,14 +1,18 @@
 module TempHelper
-  def setup_temp_file(contents)
-    stub = double
-    allow(stub).to receive(:path).and_return(random_string)
-    allow(stub).to receive(:write).with(contents)
-    allow(stub).to receive(:rewind)
+  def setup_temp_file(contents='')
+    stub = create_temp_file_stub(contents)
     allow(Tempfile).to receive(:open).and_yield(stub)
     stub
   end
 
-  protected
+  def create_temp_file_stub(contents='')
+    stub = double
+    allow(stub).to receive(:path).and_return(random_string)
+    allow(stub).to receive(:write).with(contents)
+    allow(stub).to receive(:read).and_return(contents)
+    allow(stub).to receive(:rewind)
+    stub
+  end
 
   def random_string(length=20)
     (0...length).map { (65 + rand(26)).chr }.join
