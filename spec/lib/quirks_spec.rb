9a315a00e743622eaa2eecc8700288c5ecfb3f6e
cshtdd <camilin87@gmail.com>
2020-02-05 14:30:25 -0500

    Create some tests that prove my understanding of Tempfiles

--- spec/lib/quirks_spec.rb
+++ spec/lib/quirks_spec.rb
@@ -34,3 +34,66 @@ describe 'RSpec multiple yields quirks' do
     expect(paths).to eq(['path1', 'path2'])
   end
 end
+
+describe 'Nested Temp files' do
+  it 'can be read externally' do
+    file_contents1 = "3" * 300000
+    actual = ''
+
+    Tempfile.open do |f1|
+      Tempfile.open do |f2|
+        f1.write(file_contents1)
+        f1.rewind
+
+        buffer = File.read(f1.path)
+        f2.write(buffer)
+        f2.rewind
+
+        actual = File.read(f2.path)
+      end
+    end
+
+    expect(actual).to eq(file_contents1)
+  end
+
+  it 'can be read with its handler' do
+    file_contents1 = "3" * 300000
+    actual = ''
+
+    Tempfile.open do |f1|
+      Tempfile.open do |f2|
+        f1.write(file_contents1)
+        f1.rewind
+
+        buffer = f1.read
+        f2.write(buffer)
+        f2.rewind
+
+        buffer = f2.read
+        actual << buffer
+      end
+    end
+
+    expect(actual).to eq(file_contents1)
+  end
+
+  it 'can be read mixing approaches' do
+    file_contents1 = "3" * 300000
+    actual = ''
+
+    Tempfile.open do |f1|
+      Tempfile.open do |f2|
+        f1.write(file_contents1)
+        f1.rewind
+
+        buffer = File.read(f1.path)
+        File.write(f2.path, buffer)
+
+        f2.rewind
+        actual = f2.read
+      end
+    end
+
+    expect(actual).to eq(file_contents1)
+  end
+end
