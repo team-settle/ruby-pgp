a080c02f669e2fd89c26c12283908dfeed0fe1fe
cshtdd <camilin87@gmail.com>
2020-02-05 15:58:23 -0500

    Start enhancing the script with the gpg commands

--- spec/verify.sh
+++ spec/verify.sh
@@ -2,16 +2,40 @@
 
 BASE_DIR='.'
 
-echo 'Current gpg home dir'
+function cleanup() {
+  echo 'Cleanup existing keys'
+  rspec ${BASE_DIR}/spec/lib/pgp/gpg/runner_integration_spec.rb
+
+  listKeys
+}
+
+function listKeys() {
+  echo 'List existing public keys'
+  gpg --quiet --batch --list-keys --fingerprint
+
+  echo 'List existing private keys'
+  gpg --quiet --batch --list-secret-keys --fingerprint
+}
+
+function sectionStart() {
+    echo ''
+    echo $1
+    echo '================================================'
+}
+
+function sectionEnd() {
+    echo '================================================'
+    echo ''
+}
+
+sectionStart "Setup"
 gpg --version
-
-echo 'Cleanup existing keys'
-rspec ${BASE_DIR}/spec/lib/pgp/gpg/runner_integration_spec.rb
-
-echo 'List existing public keys'
-gpg --quiet --batch --list-keys --fingerprint
-
-echo 'List existing private keys'
-gpg --quiet --batch --list-secret-keys --fingerprint
+cleanup
+sectionEnd
 
 
+sectionStart 'Decrypt key without passphrase'
+gpg --quiet --batch --import ${BASE_DIR}/spec/support/fixtures/private_key.asc
+listKeys
+cleanup
+sectionEnd
