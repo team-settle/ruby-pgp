4650f08d643b52ac2c428209741619cb1a4b08ac
cshtdd <camilin87@gmail.com>
2020-02-05 11:11:25 -0500

    Add a minor integration test to make sure the version read works as expected

--- spec/lib/pgp/gpg/runner_integration_spec.rb
+++ spec/lib/pgp/gpg/runner_integration_spec.rb
@@ -3,12 +3,16 @@ require 'spec_helper'
 describe GPG::Runner do
   include KeysHelper
 
-  let(:runner) { GPG::Runner.new }
+  let(:runner) { GPG::Runner.new(verbose: true) }
 
   before { remove_all_keys }
 
   it 'has no keys by default' do
     expect(runner.read_private_key_fingerprints).to eq([])
     expect(runner.read_public_key_fingerprints).to eq([])
+  end
+
+  it 'uses GPG version 2' do
+    expect(runner.version_default).to include('2.')
   end
 end
