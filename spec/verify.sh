6724fbc460e055968db2be0c52135a7435f14409
cshtdd <camilin87@gmail.com>
2020-02-05 16:39:30 -0500

    Now I found a solution that works on ubuntu gpg 2.2 but not on centos gpg 2.0
    Nothing a small version selection cannot do

--- spec/verify.sh
+++ spec/verify.sh
@@ -56,7 +56,7 @@ gpg --quiet --batch --import ${BASE_DIR}/spec/support/fixtures/private_key_with_
 listKeys
 echo 'Decrypting message'
 rm -f /tmp/msg1.txt
-gpg --quiet --batch --passphrase "testingpgp" --yes --ignore-mdc-error --output /tmp/msg1.txt --decrypt ${BASE_DIR}/spec/support/fixtures/encrypted_with_passphrase_key.txt.asc
+gpg --quiet --batch --pinentry-mode loopback  --passphrase "testingpgp" --yes --ignore-mdc-error --output /tmp/msg1.txt --decrypt ${BASE_DIR}/spec/support/fixtures/encrypted_with_passphrase_key.txt.asc
 printExitCode
 cat /tmp/msg1.txt
 cleanup
