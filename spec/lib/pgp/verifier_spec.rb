86acb7d9bc37904a9af4ef1ff70052bb9ab015bb
cshtdd <camilin87@gmail.com>
2020-02-07 16:00:03 -0500

    Reorganize the verifier tests in a way that simplifies their comprehension

--- spec/lib/pgp/verifier_spec.rb
+++ spec/lib/pgp/verifier_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe PGP::Verifier do
   include KeysHelper
 
-  let(:public_key_path) { Fixtures_Path.join('public_key_with_passphrase.asc').to_s }
-
   let(:verifier) { PGP::Verifier.new }
   let(:unsigned_file) { Fixtures_Path.join('signed_file.txt') }
   let(:signed_file) { Fixtures_Path.join('signed_file.txt.asc') }
@@ -12,19 +10,24 @@ describe PGP::Verifier do
   before { remove_all_keys }
 
   describe '#verify' do
-    before do
-      verifier.add_keys_from_file(public_key_path)
-    end
-
     context 'When the Public Key is from a file' do
+      let(:public_key_path) { Fixtures_Path.join('public_key_with_passphrase.asc').to_s }
+
+      before do
+        verifier.add_keys_from_file(public_key_path)
+      end
+
       it "verifies" do
         expect(verifier.verify(File.read(signed_file))).to eq(File.read(unsigned_file))
       end
     end
 
-
     context 'When the public key cannot verify a signature' do
-      let(:public_key_path) { Fixtures_Path.join('wrong_public_key_for_signature.asc').to_s }
+      let(:public_key_path_wrong_public_key) { Fixtures_Path.join('wrong_public_key_for_signature.asc').to_s }
+
+      before do
+        verifier.add_keys_from_file(public_key_path_wrong_public_key)
+      end
 
       it "should raise an exception" do
         expect {
