dcb8077afb32110d329e20c8e891e5d7ce795296
cshtdd <camilin87@gmail.com>
2020-02-07 15:50:45 -0500

    Validate there are no keys when cleaning them up

--- spec/helpers/keys_helper.rb
+++ spec/helpers/keys_helper.rb
@@ -1,5 +1,9 @@
 module KeysHelper
   def remove_all_keys
-    GPG::Engine.new.delete_all_keys
+    gpg = GPG::Engine.new
+    gpg.delete_all_keys
+
+    raise 'No public keys expected' unless gpg.runner.read_public_key_fingerprints.empty?
+    raise 'No private keys expected' unless gpg.runner.read_private_key_fingerprints.empty?
   end
 end
