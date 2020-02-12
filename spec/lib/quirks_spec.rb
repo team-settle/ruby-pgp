035470d90b3e2dea4b577be5108a1211ec156533
cshtdd <camilin87@gmail.com>
2020-02-12 15:15:19 -0500

    All the tests pass without jRuby or Bouncy Castle :clap:

--- spec/lib/quirks_spec.rb
+++ spec/lib/quirks_spec.rb
@@ -1,23 +1,26 @@
 require 'spec_helper'
 
 describe 'RSpec multiple yields quirks' do
-  it 'this should work' do
-    stub1 = double
-    allow(stub1).to receive(:path).and_return('path1')
-
-    stub2 = double
-    allow(stub2).to receive(:path).and_return('path2')
-
-    allow(Tempfile).to receive(:open).and_return(stub1, stub2)
-
-    paths = []
-    Tempfile.open do |f1|
-      Tempfile.open do |f2|
-        paths = [f1.path, f2.path]
-      end
-    end
-    expect(paths).to eq(['path1', 'path2'])
-  end
+  ##
+  ## I wrote this test to capture my lack of understanding on how to mock a method that yields multiple times
+  ##
+  #it 'this should work' do
+  #  stub1 = double
+  #  allow(stub1).to receive(:path).and_return('path1')
+  #
+  #  stub2 = double
+  #  allow(stub2).to receive(:path).and_return('path2')
+  #
+  #  allow(Tempfile).to receive(:open).and_return(stub1, stub2)
+  #
+  #  paths = []
+  #  Tempfile.open do |f1|
+  #    Tempfile.open do |f2|
+  #      paths = [f1.path, f2.path]
+  #    end
+  #  end
+  #  expect(paths).to eq(['path1', 'path2'])
+  #end
 
   it 'workaround' do
     stub1 = double
