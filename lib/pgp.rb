b942d76d2a470b368bb79ac8805dc3dfa499d038
cshtdd <camilin87@gmail.com>
2020-02-12 15:20:26 -0500

    Remove other java references

--- lib/pgp.rb
+++ lib/pgp.rb
@@ -13,18 +13,6 @@ module PGP
   autoload :RubyDecryptor,  'pgp/ruby_decryptor'
   autoload :CLI,            'pgp/cli'
 
-  #BC_Provider_Code = "BC"
-
-   # java_import 'java.io.ByteArrayInputStream'
-   # java_import 'java.security.Security'
-   # java_import 'org.bouncycastle.jce.provider.BouncyCastleProvider'
-
-  #Security.add_provider BouncyCastleProvider.new
-
-  def self.string_to_bais(string)
-    ByteArrayInputStream.new string.to_java_bytes
-  end
-
   # This exists for stubbing during tests
   def self.time_now
     Time.now
